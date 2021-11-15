16e22a1cb42b98888086e428cfa64e68e5279ff8
renuka13599 <renuka13599@gmail.com>
2021-11-15 18:55:03 +0530

    Added:Program for the peek and pop til its empthy

--- src/com/bridgelabz/Operation.java
+++ src/com/bridgelabz/Operation.java
@@ -11,4 +11,19 @@ public class Operation {
         return stacklist;
 
     }
+    public static void peek() {
+        StackQueue stacklist = new StackQueue();
+        stacklist.push(70);
+        stacklist.push(30);
+        stacklist.push(56);
+        peek();
+    }
+
+    public static void pop() {
+        System.out.println("Before Pop:");
+        StackQueue stacklist = addElement();
+        pop();
+        System.out.println("After Pop:");
+        stacklist.display();
+    }
 }
